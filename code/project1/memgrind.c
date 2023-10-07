9b8dde120ff14a4bf66a0308f95ccd3429e96f03
Kelpotronic <kelpchavan1@gmail.com>
2023-10-07 01:44:29 -0400

    minor changes

--- code/project1/memgrind.c
+++ code/project1/memgrind.c
@@ -4,11 +4,18 @@
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
 
+#ifndef TESTCASE
+#define TESTCASE 0
+#endif
+
 int main(int argc, char **argv)
 {
+
     void *init_ptr = malloc(10);
 
     void *split_1 = malloc(16);
 
     free(split_1);
+
+    free(init_ptr);
 }
