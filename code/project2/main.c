978bea23e3b49b5b6ad443975407db754a9ed930
Kelpotronic <kelpchavan1@gmail.com>
2023-11-08 12:22:42 -0500

    Updated to accomodate for behavior of commas and hyphen

--- code/project2/main.c
+++ code/project2/main.c
@@ -9,7 +9,19 @@
 
 #define BUFSIZE 1024
 
-// Create a struct to hold the word and its frequency
+/**word.c takes a directory and sorts through it finding all text files under it
+ * or other directories inside of it. It creates a linked lists of all the words
+ * found within the text files, along with a running frequency of the word being
+ * found.
+ *
+ * The final output is to print a list of all the words found within all the text
+ * files, along with the frequency (times the word was found).
+ *
+ * @Kalpesh Chavan, Hieu Nguyen
+ */
+
+/**WORD represents a word, its frequency, and a link to the next word in the linked list
+ */
 typedef struct Word
 {
     char *word;
@@ -17,23 +29,34 @@ typedef struct Word
     struct Word *next;
 } WORD;
 
+/**Dictionary creates a linked list of WORD
+ *
+ */
 typedef struct Dictionary
 {
     WORD *head;
     int size;
 } DICT;
 
+/**checks if given char is a valid letter or symbol
+ *
+ * checks if c is an A to Z letter, upper and lower case
+ * checks if c is an apostrophe
+ *
+ * @param c the char to be checked
+ * @return 1 if the char is upper or lower A to Z letter, or apostrophe
+ * @return 0 if char is not valid char
+ */
 int is_letter(char c)
 {
     // Letters include letters and apostrophe
-
     // If the character is a letter return 1
     if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
     {
         return 1;
     }
 
-    // If the character is apostrophe return 2
+    // If a character is a ', return 1
     if (c == '\'')
     {
         return 2;
@@ -42,6 +65,17 @@ int is_letter(char c)
     return 0;
 }
 
+/**processes a text file and modifies dictionary with the words in it
+ *
+ * opens a file and reads char by char
+ * checks the condition for word by the current char's following char
+ * adds word if condition is met
+ * continues until the end of the text file
+ *
+ * @param *filePath the path to the text file in directories
+ * @param *dict the dictionary containing words and frequencies
+ *
+ */
 void process_file(const char *filePath, DICT *dict)
 {
     int fd = open(filePath, O_RDONLY);
@@ -57,7 +91,6 @@ void process_file(const char *filePath, DICT *dict)
     int wordIndex = 0;
     char prevChar = '\0';
 
-    printf("Words in %s:\n", filePath);
     while ((bytesRead = read(fd, buffer, BUFSIZE)) > 0)
     {
         for (ssize_t i = 0; i < bytesRead; i++)
@@ -66,17 +99,13 @@ void process_file(const char *filePath, DICT *dict)
             {
                 word[wordIndex++] = buffer[i];
             }
-
-            else if (wordIndex > 0 && buffer[i] == '-' && i < bytesRead - 1 && is_letter(buffer[i + 1]))
+            else if (wordIndex > 0 && buffer[i] == '-' && i < bytesRead - 1 && is_letter(buffer[i + 1]) == 1)
             {
                 word[wordIndex++] = buffer[i];
             }
-
             else if (wordIndex > 0)
             {
                 word[wordIndex] = '\0';
-                printf("%s\n", word);
-                // Add the word to the dictionary
                 add_word(dict, word);
                 wordIndex = 0;
             }
@@ -88,8 +117,6 @@ void process_file(const char *filePath, DICT *dict)
         if (wordIndex > 0)
         {
             word[wordIndex] = '\0';
-            printf("%s\n", word);
-            // Add the word to the dictionary
             add_word(dict, word);
             wordIndex = 0;
         }
@@ -98,24 +125,31 @@ void process_file(const char *filePath, DICT *dict)
     if (wordIndex > 0)
     {
         word[wordIndex] = '\0';
-        printf("%s\n", word);
     }
-
     close(fd);
 }
 
+/**Adds new word or increases frequency count in dictionary
+ *
+ * takes an input word and searches through dictionary
+ * if word is found, increase its count by 1
+ * if word is not found, add it as a new word to dictionary
+ *
+ * @param *dict the dictionary containing words and frequencies
+ * @param *word the word to be added or modified in dictionary
+ * @return modifies dictionary to either add new word or increase a word count
+ *
+ */
 void add_word(DICT *dict, char *word)
 {
 
     // Try to find the word in the dictionary
     WORD *current = dict->head;
 
-    // If dict is empty, make word the head
     if (dict->head == NULL)
     {
         dict->head = (WORD *)malloc(sizeof(WORD));
         dict->head->word = strdup(word);
-        // Increment dict size
         dict->size = 1;
         dict->head->frequency = 1;
         dict->head->next = NULL;
@@ -157,7 +191,6 @@ void add_word(DICT *dict, char *word)
     {
         dict->head = newWord;
     }
-
     else
     {
         prev->next = newWord;
@@ -165,32 +198,92 @@ void add_word(DICT *dict, char *word)
 
     newWord->next = current;
 
-    // Return with the changed dictionary
     return;
 }
 
-void print_dict(DICT *dict)
+/**Finds and sorts all unique frequencies of words in dictionary
+ *
+ * First logs all of the frequencies in array
+ * sorts the entire array in decreasing order via bubble sort
+ * modifies uniq_freqs to contain only unique values in descending order
+ *
+ * @param *dict the dictionary of words and frequencies
+ * @param unique_freq the to-be modified array with all unique frequencies in dict
+ * @return modifies unique_freq to contain all unique frequencies of words in dict in descending order
+ *
+ */
+void make_unique_frequencies(DICT *dict, int unique_freq[])
 {
     WORD *current = dict->head;
+    int size = dict->size;
+    int freqs[size];
 
-    // Go through every word in the dictionary,
-    // and keep track of all of the possible frequencies,
-    // the size of the frequency array is the size of the dict
-    int freq[dict->size];
-
-
-    for (int i = 0; i < dict->size; i++)
+    // fills array with all feqs
+    for (int i = 0; i < size; i++, current = current->next)
     {
-        
-    } 
+        freqs[current->frequency]++;
+    }
 
-    while (current != NULL)
+    // sorts the array of all freqs in descending
+    for (int i = 0; i < size - 1; i++)
     {
-        printf("%s: %d\n", current->word, current->frequency);
-        current = current->next;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (freqs[i] < freqs[j])
+            {
+                // Swap the elements
+                int temp = freqs[i];
+                freqs[i] = freqs[j];
+                freqs[j] = temp;
+            }
+        }
+    }
+
+    // fills new array with only unique freqs
+    int index = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0 || freqs[i] != freqs[i - 1])
+        {
+            unique_freq[index++] = freqs[i];
+        }
     }
 }
 
+/**Prints the dictionary's words and frequencies
+ * @param the dictionary of words found
+ * @return prints the words inside dictionary based on frequency, then lexographic order
+ */
+void print_dict(DICT *dict)
+{
+
+    int unique_freq[dict->size];
+    make_unique_frequencies(dict, unique_freq);
+
+    int uniq_size = sizeof(unique_freq) / sizeof(unique_freq[0]);
+
+    for (int i = uniq_size; i >= 0; i--)
+    {
+        WORD *runner = dict->head;
+        while (runner != 0)
+        {
+            if (i == runner->frequency)
+            {
+                printf("%s: %d\n", runner->word, runner->frequency);
+            }
+            runner = runner->next;
+        }
+    }
+}
+
+/**Traverses and searches through directories for text files
+ *
+ * Uses a DFS system to find all directories and logs the text files
+ *
+ * @param *path takes the initial path directory
+ * @param *words starts the entire dictionary
+ * @return void
+ */
 void traverse(const char *path, DICT *words)
 {
 
@@ -238,6 +331,11 @@ void traverse(const char *path, DICT *words)
     }
 }
 
+/**Initializes dictionary
+ * @param starting dictionary
+ * @return void
+ *
+ */
 void init_dict(DICT *dict)
 {
     dict->head = NULL;
@@ -245,11 +343,6 @@ void init_dict(DICT *dict)
 
 int main(int argc, char *argv[])
 {
-    // if (argc != 2)
-    // {
-    //     printf("Usage: %s <path_to_directory_or_file>\n", argv[0]);
-    //     return 1;
-    // }
 
     const char *path = "testdir"; // argv[1];
 
